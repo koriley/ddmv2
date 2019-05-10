c1d2600d88ea4bc143079964bc1ece7628de777d
Kevin O'Riley <kevin@blueshorestudios.com>
2019-05-10 16:15:32 -0500

    just clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { PlayTestComponent } from './play-test/play-test.component';
